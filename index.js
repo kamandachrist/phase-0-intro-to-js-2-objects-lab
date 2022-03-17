3d018806773865ce920b39bc19c208a6d9c50f37
Christ Kamanda <kamandachristo@gmail.com>
2022-03-17 17:25:32 -0400

    Intro js objects lab

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution in this file!
+
+const newEmployee = {
+    name: 'Insert name here',
+    streetAddress: 'Insert Address here',
+  }
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+      ...employee,
+      [key]: value,
+    }
+    
+  }
+
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    const replace = {employee}
+    console.log(replace)
+    replace.employee[key] = value
+      return replace.employee
+
+    }
+  
+  function deleteFromEmployeeByKey(employee, key, value){
+    const erase = {...employee}
+    delete erase[key], value
+    return erase
+    
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key, value){
+    const erase = {employee}
+    delete erase[key], value;
+    return erase
+  }
+
+
+  
